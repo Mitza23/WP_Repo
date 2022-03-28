669f992b8599c5c4083e5f8a4c7ad0773dfa5a51
Mitza <mitzagrebla@yahoo.com>
2022-03-28 17:29:40 +0300

    Slight changes for presentation

--- Lab_4/Scripts/index.js
+++ Lab_4/Scripts/index.js
@@ -1,26 +1,21 @@
 function sortNumbers(){
     let input = document.getElementById("textInput");
     let text = input.value;
+    
+    text = text.trim().replace(/\s+/g, " ")
     let array = text.split(" ");
     array.map((x) => parseInt(x));
     array.sort((a, b) => a - b)
-    // array.forEach(function(entry) {
-    //     console.log(entry);
-    // });
+    
     let table = document.getElementById("table")
-    // for(let i = 0 ; i < array.length ; i++){
-    //     console.log(array[i]);
-    // }
-    // console.log(array.length)
     for(let i = 0 ; i <= array.length/5 ; i++){
         table.insertRow(0);
     }
-    // console.log(1/5)
+    
     for(let i = 0 ; i < array.length ; i++){
         let index = Math.floor(i/5);
         let row = table.rows[index]
         let cell = row.insertCell(-1);
-        // console.log("i: " + i)
         cell.innerHTML = array[i];
     }
 }
